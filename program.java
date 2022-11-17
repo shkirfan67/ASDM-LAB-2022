5377b7d62b1bcc16b14933e14e69f9be2206e758
shkirfan67 <shkirfan67@gmail.com>
2022-11-17 12:40:26 +0530

    done

--- program.java
+++ program.java
@@ -1,33 +1,5 @@
-import java.util.*;
-public class Empsalary {
-
-	public static void main(String[] args) 
-	{
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter the basic salary   :   ");
-        float basicsalary = s.nextInt();
-        float hra = 0;
-        float da = 0;
-        
-        if( basicsalary <= 10000)
-        {
-        	hra = basicsalary * 0.20f;
-        	da = basicsalary * 0.80f;
-        	System.out.println("salary = "+basicsalary+" "+ "HRA = "+hra+" "+ "DA  =  " + da);
-        }
-        else if( basicsalary <= 10000)
-        {
-        	hra = basicsalary * 0.20f;
-        	da = basicsalary * 0.80f;
-        	System.out.println("salary = "+basicsalary+" "+ "HRA = "+hra+" "+ "DA  =  " + da);
-        }
-        else
-        {
-            hra = basicsalary * 0.20f;
-            da = basicsalary * 0.80f;
-            System.out.println("salary = "+basicsalary+" "+ "HRA = "+hra+" "+ "DA  =  " + da);
-        }
-        s.close();
-	}
-
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
